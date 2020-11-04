17ed1f8e4354d1a2a1941b61ccd9d5ca885f4527
mauta <anna.opareva@gmail.com>
2020-11-05 00:22:28 +0300

    feat: make base

--- gem-puzzle/src/field.js
+++ gem-puzzle/src/field.js
@@ -20,62 +20,73 @@ export default class Field {
   }
 
   generate() {
+
+    let countCell = this.size * this.size;
+    const numbers = [...Array(countCell - 1).keys()]
+      .map(x => x + 1)
+    .sort(() => Math.random() - 0.5)
+
     const empty = {
+      value: countCell - 1,
       left: this.size,
       top: this.size
     }
 
-    let countCell = this.size * this.size;
     this.cells = [];
 
     const move = (item) => {
       const leftDiff = Math.abs(empty.left - item.left);
       const topDiff = Math.abs(empty.top - item.top);
       const cellMoved = item;
-      console.log("empty до")
-      console.log(empty)
-      console.log("cellMoved до")
-      console.log(cellMoved)
+      let countRight = 0
 
       if (leftDiff + topDiff > 1) {
         return;
       } else {
-
         const emptyLeft = empty.left;
         const emptyTop = empty.top;
         empty.left = cellMoved.left;
         empty.top = cellMoved.top;
-        cellMoved.left = emptyLeft
-        cellMoved.top = emptyTop
-
+        cellMoved.left = emptyLeft;
+        cellMoved.top = emptyTop;
         cellMoved.element.style.gridColumnStart = `${emptyLeft}`;
         cellMoved.element.style.gridRowStart = `${emptyTop}`;
-
-        console.log("empty ")
-        console.log(empty)
-        console.log("cellMoved ")
-        console.log(cellMoved)
       }
 
+      for (let i = 0; i < countCell - 1; i++) {
+        let position = (this.cells[i].top - 1) * this.size + this.cells[i].left;
 
+        if (position == this.cells[i].value) {
+          countRight++;
+          this.cells[i].element.style.opacity = '0.8'
+        } else {
+          this.cells[i].element.style.opacity = '1'
+        }
+
+        if (countRight === countCell - 1) {
+          this.cells.forEach(el => el.element.style.opacity = '0.1')
+        }
+      }
     }
 
     for (let i = 0; i < countCell - 1; i++) {
-      const left = i % 4 + 1;
-      const top = Math.ceil((i + 1) / 4);
-      const cell = new Cell(i + 1, this.field, top, left);
+      const left = i % this.size + 1;
+      const top = Math.ceil((i + 1) / this.size);
+      const value = numbers[i];
+      const cell = new Cell(value, this.field, top, left);
       this.cells.push({
+        value: value,
         left: left,
         top: top,
         element: this.field.lastChild
       });
     }
 
-
     this.cells.forEach(item => item.element.addEventListener('click', () => {
-      move(item)
+      move(item);
     }));
 
+    console.log(this.cells)
   }
 
 
--- gem-puzzle/src/main.js
+++ gem-puzzle/src/main.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/extensions */
 import Field from './field.js';
 
-let field = new Field();
+let field = new Field(5);
 
 field.init();
 field.generate();
